f4aaca1674139ff35bb80c7cf0c8525cd2e7812a
Yuvasri <yuvasri@Yuvasris-MacBook-Air.local>
2023-06-09 12:01:17 +0530

    first commit

---
+++ src/components/Controls.js
@@ -0,0 +1,100 @@
+import { useState, useEffect, useRef, useCallback } from 'react';
+
+// icons
+import {
+  IoPlayBackSharp,
+  IoPlayForwardSharp,
+  IoPlaySkipBackSharp,
+  IoPlaySkipForwardSharp,
+  IoPlaySharp,
+  IoPauseSharp,
+} from 'react-icons/io5';
+
+
+const Controls = ({
+  audioRef,
+  progressBarRef,
+  duration,
+  setTimeProgress,
+  tracks,
+  trackIndex,
+  setTrackIndex,
+  setCurrentTrack,
+  handleNext,
+}) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+
+  const togglePlayPause = () => {
+    setIsPlaying((prev) => !prev);
+  };
+
+  const playAnimationRef = useRef();
+
+  const repeat = useCallback(() => {
+    const currentTime = audioRef.current.currentTime;
+    setTimeProgress(currentTime);
+    progressBarRef.current.value = currentTime;
+    progressBarRef.current.style.setProperty(
+      '--range-progress',
+      `${(progressBarRef.current.value / duration) * 100}%`
+    );
+
+    playAnimationRef.current = requestAnimationFrame(repeat);
+  }, [audioRef, duration, progressBarRef, setTimeProgress]);
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+    playAnimationRef.current = requestAnimationFrame(repeat);
+  }, [isPlaying, audioRef, repeat]);
+
+  const skipForward = () => {
+    audioRef.current.currentTime += 15;
+  };
+
+  const skipBackward = () => {
+    audioRef.current.currentTime -= 15;
+  };
+
+  const handlePrevious = () => {
+    if (trackIndex === 0) {
+      let lastTrackIndex = tracks.length - 1;
+      setTrackIndex(lastTrackIndex);
+      setCurrentTrack(tracks[lastTrackIndex]);
+    } else {
+      setTrackIndex((prev) => prev - 1);
+      setCurrentTrack(tracks[trackIndex - 1]);
+    }
+  };
+
+
+  return (
+    <div className="controls-wrapper">
+      <div className="controls">
+        <button onClick={handlePrevious}>
+          <IoPlaySkipBackSharp />
+        </button>
+        <button onClick={skipBackward}>
+          <IoPlayBackSharp />
+        </button>
+
+        <button onClick={togglePlayPause}>
+          {isPlaying ? <IoPauseSharp /> : <IoPlaySharp />}
+        </button>
+        <button onClick={skipForward}>
+          <IoPlayForwardSharp />
+        </button>
+        <button onClick={handleNext}>
+          <IoPlaySkipForwardSharp />
+        </button>
+      </div>
+     
+    </div>
+  );
+};
+
+export default Controls;
